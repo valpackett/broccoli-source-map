0c6cff17c22ce8fbf2faf32f1e9a037e922118a2
myfreeweb <floatboth@me.com>
2014-07-05 14:57:45 +0400

    add extractor

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ var walkSync = require('walk-sync');
 var convert = require('convert-source-map');
 var CachingWriter = require('broccoli-caching-writer');
 
+function _genMkdirOrProcess(process) {
+	return function(relativePath) {
+		if (relativePath.slice(-1) === '/') {
+			return mkdirp.sync(path.join(destDir, relativePath));
+		} else {
+			return process(relativePath);
+		};
+	}
+}
+
 function SourceMapInliner(inputTree) {
 	if (!(this instanceof SourceMapInliner)) return new SourceMapInliner(inputTree);
 	this.inputTree = inputTree;
@@ -13,10 +23,8 @@ function SourceMapInliner(inputTree) {
 SourceMapInliner.prototype = Object.create(CachingWriter.prototype);
 SourceMapInliner.prototype.constructor = SourceMapInliner;
 SourceMapInliner.prototype.updateCache = function(srcDir, destDir) {
-	walkSync(srcDir).forEach(function(relativePath) {
-		if (relativePath.slice(-1) === '/') {
-			mkdirp.sync(path.join(destDir, relativePath));
-		} else if (relativePath.slice(-4) !== '.map') {
+	walkSync(srcDir).forEach(_genMkdirOrProcess(function(relativePath) {
+		if (relativePath.slice(-3) === '.js') {
 			var srcPath = path.join(srcDir, relativePath);
 			var destPath = path.join(destDir, relativePath);
 			var srcCode = fs.readFileSync(srcPath, {encoding: 'utf-8'});
@@ -33,9 +41,34 @@ SourceMapInliner.prototype.updateCache = function(srcDir, destDir) {
 				fs.writeFileSync(destPath, srcCode);
 			}
 		}
-	});
+	}));
+}
+
+function SourceMapExtractor(inputTree) {
+	if (!(this instanceof SourceMapExtractor)) return new SourceMapExtractor(inputTree);
+	this.inputTree = inputTree;
+}
+SourceMapExtractor.prototype = Object.create(CachingWriter.prototype);
+SourceMapExtractor.prototype.constructor = SourceMapExtractor;
+SourceMapExtractor.prototype.updateCache = function(srcDir, destDir) {
+	walkSync(srcDir).forEach(_genMkdirOrProcess(function(relativePath) {
+		if (relativePath.slice(-3) === '.js') {
+			var srcPath = path.join(srcDir, relativePath);
+			var destPath = path.join(destDir, relativePath);
+			var srcCode = fs.readFileSync(srcPath, {encoding: 'utf-8'});
+			var smap = convert.fromComment(srcCode, srcDir);
+			console.log(relativePath);
+			if (smap !== null) {
+				fs.writeFileSync(destPath, convert.removeComments(srcCode) + '\n//# sourceMappingURL=' + relativePath + '.map');
+				fs.writeFileSync(destPath + '.map', smap.toJSON());
+			} else {
+				fs.writeFileSync(destPath, srcCode);
+			}
+		}
+	}));
 }
 
 module.exports = {
-	inline: SourceMapInliner
+	inline: SourceMapInliner,
+	extract: SourceMapExtractor
 }
