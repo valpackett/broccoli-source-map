0ecf749157642239af7d7608bf35c88180e5058d
Greg V <greg@unrelenting.technology>
2015-08-02 22:22:35 +0300

    kill semicolons

--- index.js
+++ index.js
@@ -1,80 +1,81 @@
-'use strict';
-var fs = require('fs');
-var path = require('path');
-var mkdirp = require('mkdirp');
-var walkSync = require('walk-sync');
-var convert = require('convert-source-map');
-var CachingWriter = require('broccoli-caching-writer');
+'use strict'
+
+var fs = require('fs')
+var path = require('path')
+var mkdirp = require('mkdirp')
+var walkSync = require('walk-sync')
+var convert = require('convert-source-map')
+var CachingWriter = require('broccoli-caching-writer')
 
 function _genMkdirOrProcess(destDir, process) {
 	return function(relativePath) {
 		if (relativePath.slice(-1) === '/') {
-			return mkdirp.sync(path.join(destDir, relativePath));
+			return mkdirp.sync(path.join(destDir, relativePath))
 		} else {
-			return process(relativePath);
+			return process(relativePath)
 		}
 	}
 }
 
 function _isSourceMappableFile(path) {
 	return path.slice(-3) === '.js'
-		|| path.slice(-4) === '.css';
+		|| path.slice(-4) === '.css'
 }
 
 var SourceMapProcessor = CachingWriter.extend({
 	updateCache: function(srcPaths, destDir) {
-		var self = this;
+		var self = this
 		srcPaths.forEach(function(srcDir) {
 			walkSync(srcDir).forEach(_genMkdirOrProcess(destDir, function(relativePath) {
 				if (_isSourceMappableFile(relativePath)) {
-					var srcPath = path.join(srcDir, relativePath);
-					var destPath = path.join(destDir, relativePath);
-					var srcCode = fs.readFileSync(srcPath, {encoding: 'utf-8'});
-					self.processCode(srcCode, path.dirname(srcPath), destPath, relativePath);
+					var srcPath = path.join(srcDir, relativePath)
+					var destPath = path.join(destDir, relativePath)
+					var srcCode = fs.readFileSync(srcPath, {encoding: 'utf-8'})
+					self.processCode(srcCode, path.dirname(srcPath), destPath, relativePath)
 				}
-			}));
-		});
+			}))
+		})
 	}
-});
+})
 
 var SourceMapInliner = SourceMapProcessor.extend({
 	processCode: function(srcCode, srcDir, destPath, relativePath) {
-		var smap = convert.fromMapFileSource(srcCode, srcDir);
+		var smap = convert.fromMapFileSource(srcCode, srcDir)
 		if (smap !== null && typeof smap['sourcemap'] !== 'undefined') {
 			if (typeof smap.getProperty('sourcesContent') === 'undefined' && typeof smap.getProperty('sources') !== 'undefined') {
 				var contents = smap.getProperty('sources').map(function(spath) {
-					return fs.readFileSync(path.join(srcDir, spath), {encoding: 'utf-8'});
-				});
-				smap = smap.setProperty('sourcesContent', contents);
+					return fs.readFileSync(path.join(srcDir, spath), {encoding: 'utf-8'})
+				})
+				smap = smap.setProperty('sourcesContent', contents)
 			}
-			var comment = smap.toComment();
+			var comment = smap.toComment()
 			if (destPath.slice(-4) === '.css') {
-				comment = comment.replace(/^\/\//, '/*') + ' */';
+				comment = comment.replace(/^\/\//, '/*') + ' */'
 			}
-			fs.writeFileSync(destPath, convert.removeMapFileComments(srcCode) + '\n' + comment);
+			fs.writeFileSync(destPath, convert.removeMapFileComments(srcCode) + '\n' + comment)
 		} else {
-			fs.writeFileSync(destPath, srcCode);
+			fs.writeFileSync(destPath, srcCode)
 		}
 	}
-});
+})
 
 var SourceMapExtractor = SourceMapProcessor.extend({
 	processCode: function(srcCode, srcDir, destPath, relativePath) {
-		var smap = convert.fromComment(srcCode, srcDir);
+		var smap = convert.fromComment(srcCode, srcDir)
 		if (smap !== null) {
-			var comment = '//# sourceMappingURL=' + path.basename(relativePath) + '.map';
+			var comment = '//# sourceMappingURL=' + path.basename(relativePath) + '.map'
 			if (destPath.slice(-4) === '.css') {
-				comment = '/*# sourceMappingURL=' + path.basename(relativePath) + '.map */';
+				comment = '/*# sourceMappingURL=' + path.basename(relativePath) + '.map */'
 			}
-			fs.writeFileSync(destPath, convert.removeComments(srcCode) + '\n' + comment);
-			fs.writeFileSync(destPath + '.map', smap.toJSON());
+			fs.writeFileSync(destPath, convert.removeComments(srcCode) + '\n' + comment)
+			fs.writeFileSync(destPath + '.map', smap.toJSON())
 		} else {
-			fs.writeFileSync(destPath, srcCode);
+			fs.writeFileSync(destPath, srcCode)
 		}
 	}
-});
+})
 
 module.exports = {
-	inline: function(t, o) { return new SourceMapInliner(t, o); },
-	extract: function(t, o) { return new SourceMapExtractor(t, o); }
+	inline: function(t, o) { return new SourceMapInliner(t, o) },
+	extract: function(t, o) { return new SourceMapExtractor(t, o) }
 }
--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var path = require('path');
-var expect = require('expect.js');
-var broccoli = require('broccoli');
-var sourceMap = require('..');
+var fs = require('fs')
+var path = require('path')
+var expect = require('expect.js')
+var broccoli = require('broccoli')
+var sourceMap = require('..')
 
-function fixt(n) { return path.join('test/fixtures', n); }
+function fixt(n) { return path.join('test/fixtures', n) }
 
 describe('sourceMap', function() {
 	it('inlines', function() {
@@ -16,9 +16,9 @@ describe('sourceMap', function() {
 					fs.readFileSync(path.join(result.directory, 'example-es6.js'), {encoding: 'utf8'}).replace('\n\n\n\n', '\n\n')
 				).to.eql(
 					fs.readFileSync(fixt('compiled-as-inline/example-es6.js'), {encoding: 'utf8'})
-				);
-			});
-	});
+				)
+			})
+	})
 
 	it('extracts', function() {
 		return new broccoli.Builder(sourceMap.extract(fixt('compiled-as-inline')))
@@ -27,12 +27,12 @@ describe('sourceMap', function() {
 					fs.readFileSync(path.join(result.directory, 'example-es6.js'), {encoding: 'utf8'}).replace('\n\n\n', '\n\n') + '\n'
 				).to.eql(
 					fs.readFileSync(fixt('compiled-as-external/example-es6.js'), {encoding: 'utf8'})
-				);
+				)
 				expect(
 					fs.readFileSync(path.join(result.directory, 'example-es6.js.map'), {encoding: 'utf8'})
 				).to.eql(
 					fs.readFileSync(fixt('compiled-as-external/example-es6.js.map'), {encoding: 'utf8'})
-				);
-			});
-	});
-});
+				)
+			})
+	})
+})
