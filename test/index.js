484c9748684bcdf128b4f0b611ebf154868d582d
Greg V <greg@unrelenting.technology>
2015-08-02 22:26:29 +0300

    use tape

--- test/index.js
+++ test/index.js
@@ -2,37 +2,34 @@
 
 var fs = require('fs')
 var path = require('path')
-var expect = require('expect.js')
+var test = require('tape')
 var broccoli = require('broccoli')
-var sourceMap = require('..')
+var sourceMap = require('../')
 
 function fixt(n) { return path.join('test/fixtures', n) }
 
-describe('sourceMap', function() {
-	it('inlines', function() {
-		return new broccoli.Builder(sourceMap.inline(fixt('compiled-as-external')))
-			.build().then(function(result) {
-				expect(
-					fs.readFileSync(path.join(result.directory, 'example-es6.js'), {encoding: 'utf8'}).replace('\n\n\n\n', '\n\n')
-				).to.eql(
-					fs.readFileSync(fixt('compiled-as-inline/example-es6.js'), {encoding: 'utf8'})
-				)
-			})
-	})
+test('inline', function(t) {
+	t.plan(1)
+	new broccoli.Builder(sourceMap.inline(fixt('compiled-as-external')))
+		.build().then(function(result) {
+			t.equal(
+				fs.readFileSync(path.join(result.directory, 'example-es6.js'), {encoding: 'utf8'}).replace('\n\n\n\n', '\n\n'),
+				fs.readFileSync(fixt('compiled-as-inline/example-es6.js'), {encoding: 'utf8'})
+			)
+		})
+})
 
-	it('extracts', function() {
-		return new broccoli.Builder(sourceMap.extract(fixt('compiled-as-inline')))
-			.build().then(function(result) {
-				expect(
-					fs.readFileSync(path.join(result.directory, 'example-es6.js'), {encoding: 'utf8'}).replace('\n\n\n', '\n\n') + '\n'
-				).to.eql(
-					fs.readFileSync(fixt('compiled-as-external/example-es6.js'), {encoding: 'utf8'})
-				)
-				expect(
-					fs.readFileSync(path.join(result.directory, 'example-es6.js.map'), {encoding: 'utf8'})
-				).to.eql(
-					fs.readFileSync(fixt('compiled-as-external/example-es6.js.map'), {encoding: 'utf8'})
-				)
-			})
-	})
+test('extract', function(t) {
+	t.plan(2)
+	new broccoli.Builder(sourceMap.extract(fixt('compiled-as-inline')))
+		.build().then(function(result) {
+			t.equal(
+				fs.readFileSync(path.join(result.directory, 'example-es6.js'), {encoding: 'utf8'}).replace('\n\n\n', '\n\n') + '\n',
+				fs.readFileSync(fixt('compiled-as-external/example-es6.js'), {encoding: 'utf8'})
+			)
+			t.equal(
+				fs.readFileSync(path.join(result.directory, 'example-es6.js.map'), {encoding: 'utf8'}),
+				fs.readFileSync(fixt('compiled-as-external/example-es6.js.map'), {encoding: 'utf8'})
+			)
+		})
 })
